cbc61c76880c3061a1019e4b1d707f31549d55be
Yuno <yunonishi092809@gmail.com>
2026-01-17 20:14:30 +0900

    Initial Commit

---
+++ original/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  original
+//
+//  Created by 西村 on 2026/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ original/originalApp.swift
@@ -0,0 +1,17 @@
+//
+//  originalApp.swift
+//  original
+//
+//  Created by 西村 on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct originalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
